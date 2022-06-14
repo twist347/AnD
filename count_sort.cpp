e74bfd9c02a30c9469b40eb0bcdb26d340c44257
twist <postpost42@yandex.ru>
2022-06-14 21:59:34 +0300

    count_sort.cpp

---
+++ count_sort.cpp
@@ -0,0 +1,20 @@
+void count_sort(int *arr, int N) {
+    int max = -1;
+    for (int i = 0; i < N; ++i) {
+        if (arr[i] > max) max = arr[i];
+    }
+    int t[max + 1] = {0};
+    for (int i = 0; i < max + 1; ++i) {
+        t[i] = 0;
+    }
+    for (int i = 0; i < N; ++i) {
+        t[arr[i]]++;
+    }
+    int k = 0;
+    for (int i = 0; i < max + 1; ++i) {
+        for (int j = 0; j < t[i]; ++j) {
+            arr[k] = i;
+            ++k;
+        }
+    }
+}
