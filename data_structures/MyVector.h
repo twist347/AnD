2aeebe68197ac751dca522349545376e92e8ee92
twist347 <70428550+twist347@users.noreply.github.com>
2022-07-15 11:04:49 +0300

    Update MyVector.h

--- data_structures/MyVector.h
+++ data_structures/MyVector.h
@@ -3,53 +3,148 @@
 
 #include <algorithm>
 
+
 template<typename T>
 class MyVector {
 public:
-    explicit MyVector(int sz = 0);
+    explicit MyVector(int sz = 0) : m_size(sz), m_capacity(sz + ADDITIONAL_CAPACITY) {
+        m_buffer = new T[m_capacity];
+    }
 
-    MyVector(const MyVector &mv);
+    MyVector(const MyVector &mv) : m_size(mv.m_size), m_capacity(mv.m_capacity) {
+        m_buffer = new T[m_capacity];
+        for (int i = 0; i < m_size; i++) {
+            m_buffer[i] = mv.m_buffer[i];
+        }
+    }
 
-    MyVector(MyVector &&mv) noexcept;
+    MyVector(MyVector &&mv) noexcept: m_size(mv.m_size), m_capacity(mv.m_capacity), m_buffer(mv.m_buffer) {
+        mv.m_buffer = nullptr;
+        mv.m_size = 0;
+        mv.m_capacity = 0;
+    }
 
-    ~MyVector();
+    ~MyVector() {
+        delete[] m_buffer;
+    }
 
-    MyVector &operator=(const MyVector &mv);
+    MyVector &operator=(const MyVector &mv) {
+        if (this == &mv) {
+            return *this;
+        }
 
-    MyVector &operator=(MyVector &&mv) noexcept;
+        m_size = mv.m_size;
+        m_capacity = mv.m_capacity;
+        delete[] m_buffer;
+        m_buffer = new T[m_size];
+        for (int i = 0; i < m_size; i++) {
+            m_buffer[i] = mv.m_buffer[i];
+        }
 
-    void reserve(int newCapacity);
+        // MyVector t = mv;
+        // std::swap(*this, copy);
+        return *this;
+    }
 
-    void resize(int newSize);
+    MyVector &operator=(MyVector &&mv) noexcept {
+        m_size = mv.m_size;
+        m_capacity = mv.m_capacity;
+        m_buffer = mv.m_buffer;
 
-    T &operator[](int idx);
+        mv.m_buffer = nullptr;
+        mv.m_size = 0;
+        mv.m_capacity = 0;
+        // std::swap(m_size, mv.m_size);
+        // std::swap(m_capacity, mv.m_capacity);
+        // std::swap(m_buffer, mv.m_buffer);
+        return *this;
+    }
 
-    const T &operator[](int idx) const;
+    void reserve(int newCapacity) {
+        if (newCapacity < m_size) {
+            return;
+        }
 
-    bool empty() const;
+        T *tmp = new T[newCapacity];
+        for (int i = 0; i < m_size; ++i) {
+            tmp[i] = std::move(m_buffer[i]);
+        }
 
-    int size() const;
+        m_capacity = newCapacity;
+//        std::swap(m_buffer, tmp);
 
-    int capacity() const;
+        delete[] m_buffer;
+        m_buffer = tmp;
+//        delete[] tmp;
+    }
 
-    void push_back(const T &t);
+    void resize(int newSize) {
+        if (newSize > m_capacity) {
+            reserve(newSize * 2);
+        }
+        m_size = newSize;
+    }
 
-    void push_back(T &&t);
+    T &operator[](int idx) {
+        return m_buffer[idx];
+    }
 
-    void pop_back();
+    const T &operator[](int idx) const {
+        return m_buffer[idx];
+    }
 
-    const T &back() const;
+    bool empty() const {
+        return m_size == 0;
+    }
+
+    int size() const {
+        return m_size;
+    }
+
+    int capacity() const {
+        return m_capacity;
+    }
+
+    void push_back(const T &t) {
+        if (m_size == m_capacity) {
+            reserve(2 * m_capacity);
+        }
+        m_buffer[m_size++] = t;
+    }
+
+    void push_back(T &&t) {
+        if (m_size == m_capacity) {
+            reserve(2 * m_capacity);
+        }
+        m_buffer[m_size++] = std::move(t);
+    }
+
+    void pop_back() {
+        --m_size;
+    }
+
+    const T &back() const {
+        return m_buffer[m_size - 1];
+    }
 
     typedef T *iterator;
     typedef const T *const_iterator;
 
-    iterator begin();
+    iterator begin() {
+        return &m_buffer[0];
+    }
 
-    iterator end();
+    iterator end() {
+        return &m_buffer[m_size];
+    }
 
-    const_iterator cbegin() const;
+    const_iterator cbegin() const {
+        return &m_buffer[0];
+    }
 
-    const_iterator cend() const;
+    const_iterator cend() const {
+        return &m_buffer[m_size];
+    }
 
     static const int ADDITIONAL_CAPACITY = 16;
 
@@ -58,163 +153,6 @@ private:
     int m_capacity;
     T *m_buffer;
 };
-
-template<typename T>
-MyVector<T>::MyVector(int sz) : m_size(sz), m_capacity(sz + ADDITIONAL_CAPACITY) {
-    m_buffer = new T[m_capacity];
-}
-
-template<typename T>
-MyVector<T>::MyVector(const MyVector &mv) : m_size(mv.m_size), m_capacity(mv.m_capacity) {
-    m_buffer = new T[m_capacity];
-    for (int i = 0; i < m_size; i++) {
-        m_buffer[i] = mv.m_buffer[i];
-    }
-}
-
-template<typename T>
-MyVector<T>::MyVector(MyVector &&mv) noexcept : m_size(mv.m_size), m_capacity(mv.m_capacity), m_buffer(mv.m_buffer) {
-    mv.m_buffer = nullptr;
-    mv.m_size = 0;
-    mv.m_capacity = 0;
-}
-
-template<typename T>
-MyVector<T>::~MyVector() {
-    delete[] m_buffer;
-}
-
-template<typename T>
-MyVector<T> &MyVector<T>::operator=(const MyVector &mv) {
-    if (this == &mv) {
-        return *this;
-    }
-
-    m_size = mv.m_size;
-    m_capacity = mv.m_capacity;
-    delete[] m_buffer;
-    m_buffer = new T[m_size];
-    for (int i = 0; i < m_size; i++) {
-        m_buffer[i] = mv.m_buffer[i];
-    }
-
-    // MyVector t = mv;
-    // std::swap(*this, copy);
-    return *this;
-}
-
-template<typename T>
-MyVector<T> &MyVector<T>::operator=(MyVector &&mv) noexcept {
-    m_size = mv.m_size;
-    m_capacity = mv.m_capacity;
-    m_buffer = mv.m_buffer;
-
-    mv.m_buffer = nullptr;
-    mv.m_size = 0;
-    mv.m_capacity = 0;
-    // std::swap(m_size, mv.m_size);
-    // std::swap(m_capacity, mv.m_capacity);
-    // std::swap(m_buffer, mv.m_buffer);
-    return *this;
-}
-
-template<typename T>
-void MyVector<T>::reserve(int newCapacity) {
-    if (newCapacity < m_size) {
-        return;
-    }
-
-    T *tmp = new T[newCapacity];
-    for (int i = 0; i < m_size; ++i) {
-        tmp[i] = std::move(m_buffer[i]);
-    }
-
-    m_capacity = newCapacity;
-//        std::swap(m_buffer, tmp);
-
-    delete[] m_buffer;
-    m_buffer = tmp;
-//        delete[] tmp;
-}
-
-template<typename T>
-void MyVector<T>::resize(int newSize) {
-    if (newSize > m_capacity) {
-        reserve(newSize * 2);
-    }
-    m_size = newSize;
-}
-
-template<typename T>
-T &MyVector<T>::operator[](int idx) {
-    return m_buffer[idx];
-}
-
-template<typename T>
-const T &MyVector<T>::operator[](int idx) const {
-    return m_buffer[idx];
-}
-
-template<typename T>
-bool MyVector<T>::empty() const {
-    return m_size == 0;
-}
-
-template<typename T>
-int MyVector<T>::size() const {
-    return m_size;
-}
-
-template<typename T>
-int MyVector<T>::capacity() const {
-    return m_capacity;
-}
-
-template<typename T>
-void MyVector<T>::push_back(const T &t) {
-    if (m_size == m_capacity) {
-        reserve(2 * m_capacity);
-    }
-    m_buffer[m_size++] = t;
-}
-
-template<typename T>
-void MyVector<T>::push_back(T &&t) {
-    if (m_size == m_capacity) {
-        reserve(2 * m_capacity);
-    }
-    m_buffer[m_size++] = std::move(t);
-}
-
-template<typename T>
-void MyVector<T>::pop_back() {
-    --m_size;
-}
-
-template<typename T>
-const T &MyVector<T>::back() const {
-    return m_buffer[m_size - 1];
-}
-
-template<typename T>
-T *MyVector<T>::begin() {
-    return &m_buffer[0];
-}
-
-template<typename T>
-T *MyVector<T>::end() {
-    return &m_buffer[m_size];
-}
-
-template<typename T>
-const T *MyVector<T>::cbegin() const {
-    return &m_buffer[0];
-}
-
-template<typename T>
-const T *MyVector<T>::cend() const {
-    return &m_buffer[m_size];
-}
 
 
 #endif //AND_MYVECTOR_H
